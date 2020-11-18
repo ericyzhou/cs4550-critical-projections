e43bcb5606c82c74b9d769906449be89dfa4e841
Eric <ericyzhou00@gmail.com>
2020-11-17 21:29:58 -0500

    search enter button

--- critical-projections-client-react/src/components/IMDBSearchListComponent.js
+++ critical-projections-client-react/src/components/IMDBSearchListComponent.js
@@ -5,6 +5,7 @@ import {Link} from "react-router-dom";
 class IMDBSearchListComponent extends React.Component {
 
     state = {
+        searched: false,
         searchTerm: "",
         results: {}
     }
@@ -20,8 +21,15 @@ class IMDBSearchListComponent extends React.Component {
         imdbSearchService.findMoviesByTitle(movieTitle)
             .then(results => this.setState(prevState => ({
                 ...this.state,
+                searched: true,
                 results
             })))
+    }
+
+    handleKeyPress = (target) => {
+        if(target.charCode==13) {
+            this.searchMovie(this.state.searchTerm)
+        }
     }
 
     constructor(props) {
@@ -29,48 +37,70 @@ class IMDBSearchListComponent extends React.Component {
     }
 
     componentDidMount() {
+        this.state.searched = false
     }
 
     render() {
         return(
             <div className={"container"}>
                 <h1>Search</h1>
-                <input className={"form-control"}
-                       placeholder={"Movie name"}
-                       onChange={(event) => this.updateSearchTerm(event.target.value)}/>
-                <button
-                    className={"btn btn-primary"}
-                    onClick={() => this.searchMovie(this.state.searchTerm)}>
-                    Search
-                </button>
-                {
-                    this.state.results.Search &&
-                    <table className={"table"}>
-                        <thead>
-                        <tr>
-                            <th scope={"col"}>Title</th>
-                            <th scope={"col"}>Year Released</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.state.results.Search.map(result =>
-                                <tr>
-                                    <td><Link to={`/search/movies/${result.imdbID}`}>{result.Title}</Link></td>
-                                    <td>{result.Year}</td>
-                                </tr>
-                            )
-                        }
-                        </tbody>
-                    </table>
-                }
-                {
-                    !this.state.results.Search &&
-                    <h3>
-                        No movies found
-                    </h3>
-                }
-
+                <div className={"input-group"}>
+                    <input type={"text"}
+                           className={"form-control"}
+                           placeholder={"Movie name"}
+                           onChange={(event) => this.updateSearchTerm(event.target.value)}
+                           onKeyPress={this.handleKeyPress}/>
+                    <div className={"input-group-append"}>
+                        <button type={"button"}
+                                className={"btn btn-primary"}
+                                onClick={() => this.searchMovie(this.state.searchTerm)}>
+                            Search
+                        </button>
+                    </div>
+                </div>
+                <div>
+                    {
+                        this.state.searched &&
+                        <div>
+                            {
+                                this.state.results.Search &&
+                                <div>
+                                    <h6>{this.state.results.Search.length} results found</h6>
+                                    <table className={"table"}>
+                                        <thead>
+                                        <tr>
+                                            <th scope={"col"}>Title</th>
+                                            <th scope={"col"}>Year Released</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {
+                                            this.state.results.Search.map(result =>
+                                                <tr>
+                                                    <td><Link
+                                                        to={`/search/movies/${result.imdbID}`}>{result.Title}</Link>
+                                                    </td>
+                                                    <td>{result.Year}</td>
+                                                </tr>
+                                            )
+                                        }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            }
+                            {
+                                !this.state.results.Search &&
+                                <h3>
+                                    No results
+                                </h3>
+                            }
+                        </div>
+                    }
+                    {
+                        !this.state.searched &&
+                        <div></div>
+                    }
+                </div>
             </div>
         )
     }
