7dbd476600ddfeb28693a96a72421142f22c1db3
Eric <ericyzhou00@gmail.com>
2020-11-19 10:49:00 -0500

    review

--- critical-projections-client-react/src/index.js
+++ critical-projections-client-react/src/index.js
@@ -9,17 +9,30 @@ import {BrowserRouter, Route, Link} from "react-router-dom";
 import MovieComponent from "./components/MovieComponent";
 import ReviewsComponent from './components/ReviewsComponent'
 import HomePageContainer from "./containers/HomePageContainer";
+import {LoginComponent} from "./components/LoginComponent";
+import {ProfileComponent} from "./components/ProfileComponent";
 
 ReactDOM.render(
     <BrowserRouter>
         <div>
             <Link to={'/search/movies'}>Search Movies</Link>
             <br/>
+<<<<<<< HEAD
             <Link to={'/'}>Home</Link>
             <Route path={'/'} exact component={HomePageContainer}/>
+=======
+            <Link to={'/home'}>Home</Link>
+            <br/>
+            <Link to={'/login'}>Login</Link>
+            <br/>
+            <Link to={'/profile'}>Profile</Link>
+            <Route path={'/home'} exact component={HomePageContainer}/>
+>>>>>>> e9620d629aba6e06405f846ae0012921ee550d8b
             <Route path={'/search/movies'} exact component={IMDBSearchListComponent}/>
             <Route path={'/search/movies/:movieId'} exact component={MovieComponent}/>
             <Route path={'/search/movies/:movieId/reviews'} exact component={ReviewsComponent}/>
+            <Route path={'/login'} exact component={LoginComponent}/>
+            <Route path={'/profile'} exact component={ProfileComponent}/>
         </div>
     </BrowserRouter>,
   document.getElementById('root')
